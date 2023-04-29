dc534cd18042be0ed3075eb8a45eb8d26f3894bf
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-04-29 20:28:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// alert is a box that pops up to give the user a messsgae //
+//example:
+// alert("Thanks for your input!");
+
+
+//some corders also use window.alerts instead of simply alert//
+//example:
+// window.alert ("Hey Welcome here!");
+
+
+// window.alert ("I Hope It Should be useful for you!");
+
+
+// window.alert ("uh!");
+
+
+// we can also break line in alert box by using \n keyword//
+// example:
+// window.alert ("hey welcome to js land\nhappy coding")
+
+
+// we can also use two alerts to show two messages and the second alerts box will pop up after the first one //
+//example:
+window.alert ("It's good to see you here!");
+window.alert ("Happy Coding!");
